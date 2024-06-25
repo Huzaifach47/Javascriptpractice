48960fe048acc1ccad8a4d658e6df5119a79d347
huzaifa ch <huzaifa.khuzaima47@gmail.com>
2024-06-25 12:21:07 +0500

    practice files

---
+++ practice.js
@@ -0,0 +1,5 @@
+let country = "Pakistan";
+let province = "Punjab";
+let city = "Lahore";
+let output = "lahore," + "punjab" + " " + "is" + " " + "in" + " " + "pakistan";
+alert(output);
---
+++ practice2.js
@@ -0,0 +1,8 @@
+let x = 3;
+let y = 5;
+
+if (5 > 3) {
+  alert("5 is greater than 3");
+} else {
+  alert("3 is greater than 4");
+}
