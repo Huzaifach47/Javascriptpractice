32994bfe4979d47ee160ae1e8ff67a7259de6aa9
huzaifa ch <huzaifa.khuzaima47@gmail.com>
2024-06-25 13:32:23 +0500

    else-if enhancement

--- practice2.js
+++ practice2.js
@@ -3,6 +3,8 @@ let y = 5;
 
 if (x > y) {
   alert(x + " is greater than " + y);
+} else if (x === y) {
+  alert(x + " is equal to " + y);
 } else {
-  alert(y + " is greater than " + x);
+  alert(x + " is smaller than " + y);
 }
