c1a319783b29a5fa3936f2d52fe2c20ac71fa62e
huzaifa ch <huzaifa.khuzaima47@gmail.com>
2024-06-26 16:12:27 +0500

    assignment3-

--- assignment3.js
+++ assignment3.js
@@ -1,14 +1,14 @@
-let bill = 40;
-let usualTip = (15 / 100) * 40;
-let tip = (20 / 100) * 40;
+let bill = 400;
+let usualTip = (15 / 100) * bill;
+let tip = (20 / 100) * bill;
 let usualtotalValue = bill + usualTip;
 let totalValue = bill + tip;
 
-if (bill > 50 || bill < 300) {
+if (bill > 50 && bill < 300) {
   console.log(
-    `The bill was ${bill}, the tip was ${usualTip}, and the total value ${usualtotalValue}`
+    `Hi The bill was ${bill}, the tip was ${usualTip}, and the total value ${usualtotalValue}`
   );
-} else if (bill <= 50 || bill >= 300) {
+} else {
   console.log(
     `The bill was ${bill}, the tip was ${tip}, and the total value ${totalValue}`
   );
