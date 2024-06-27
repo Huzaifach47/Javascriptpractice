fc25409efd29a355599c13edc5b6a1b7a7fc4807
huzaifa ch <huzaifa.khuzaima47@gmail.com>
2024-06-27 14:24:32 +0500

    calc with functions

--- calculator.js
+++ calculator.js
@@ -1,20 +1,33 @@
-let x = parseInt(prompt("Enter a number"));
-let y = parseInt(prompt("Enter a second number"));
+let firstNum = parseInt(prompt("Enter a number"));
+let SecondNum = parseInt(prompt("Enter a second number"));
 
 let operator = prompt("Choose : +  -  *  /");
 
+function add(firstNum, SecondNum) {
+  return firstNum + SecondNum;
+}
+function subt(firstNum, SecondNum) {
+  return firstNum - SecondNum;
+}
+function mul(firstNum, SecondNum) {
+  return firstNum * SecondNum;
+}
+function div(firstNum, SecondNum) {
+  return firstNum / SecondNum;
+}
+
 if (operator === "+") {
-  let sum = x + y;
+  let sum = add(firstNum, SecondNum);
   alert(`Your answer is ${sum}`);
 } else if (operator === "-") {
-  let subt = x - y;
-  alert(`Your answer is ${subt}`);
+  add(firstNum, SecondNum);
+  alert(`Your answer is ${add}`);
 } else if (operator === "*") {
-  let mul = x * y;
-  alert(`Your answer is ${mul}`);
+  add(firstNum, SecondNum);
+  alert(`Your answer is ${add}`);
 } else if (operator === "/") {
-  let div = x / y;
-  alert(`Your answer is ${div}`);
+  add(firstNum, SecondNum);
+  alert(`Your answer is ${add}`);
 } else {
   alert("Wrong input");
 }
