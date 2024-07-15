ad96fdde52d103de33a0b3a5eb5c8da5af0a825e
huzaifa ch <huzaifa.khuzaima47@gmail.com>
2024-07-15 10:27:00 +0500

    array

---
+++ arrays.js
@@ -0,0 +1,18 @@
+const names = [
+  "huzaifa",
+  "naveed",
+  "amjad",
+  "ali",
+  "taha",
+  "hamza",
+  "abyaz",
+  "bilal",
+  "ahmed",
+  "anas",
+];
+
+for (let i = 0; i < 10; i++) {
+  let name = names[i];
+  let chrcLength = name.length;
+  console.log(`${name} has ${chrcLength} characters`);
+}
