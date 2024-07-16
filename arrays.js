70ae7a26e2ef0f8a63fc7f5d4cfbf7088b6e9535
huzaifa ch <huzaifa.khuzaima47@gmail.com>
2024-07-16 10:41:15 +0500

    arrays-methods

--- arrays.js
+++ arrays.js
@@ -11,8 +11,23 @@ const names = [
   "anas",
 ];
 
-for (let i = 0; i < 10; i++) {
-  let name = names[i];
-  let chrcLength = name.length;
-  console.log(`${name} has ${chrcLength} characters`);
-}
+// for (let i = 0; i < 10; i++) {
+//   let name = names[i];
+//   let chrcLength = name.length;
+//   console.log(`${name} has ${chrcLength} characters`);
+// }
+
+// // ****join-method*****
+// console.log(names.join("-"));
+
+// // ****pop-method*****
+// console.log(names.pop());
+
+// // ****push-method*****
+// console.log(names.push("salt"));
+
+// // ****shift-method*****
+// console.log(names.shift());
+
+// // ****unshift-method*****
+// console.log(names.unshift("salt"));
