e8da77463c4dc2443d57b54a31dc742678036cdd
huzaifa ch <huzaifa.khuzaima47@gmail.com>
2024-07-17 11:37:54 +0500

    for-loops

---
+++ assignment8.js
@@ -0,0 +1,47 @@
+// ******RIGHTANGLE TRIANGLE*****
+
+// let star = "*";
+// let steric = "#";
+// let starOutput = "";
+// let stericOutput = "";
+// let finalOutput = "";
+// for (let i = 1; i <= 5; i++) {
+//   starOutput = starOutput + star;
+//   finalOutput = finalOutput + "\n" + starOutput;
+// }
+// console.log(finalOutput);
+
+let star = "*";
+let steric = "#";
+let starOutput = "";
+let stericOutput = "";
+let finalOutput = "";
+for (let k = 5; k >= 1; k--) {
+  for (let i = 1; i <= k; i++) {
+    finalOutput = finalOutput + star;
+  }
+  finalOutput = finalOutput + "\n";
+}
+console.log(finalOutput);
+// for (let j = 1; j <= 10; j++) {
+//   for (let i = 1; i <= 6; i++) {
+//     if (j % 2 == 1) {
+//       finalOutput = finalOutput + star;
+//     } else {
+//       finalOutput = finalOutput + steric;
+//     }
+//   }
+//   finalOutput = finalOutput + "\n";
+// }
+// console.log(finalOutput);
+
+// let star = "******";
+// let steric = "######";
+
+// let finalOutput = "";
+
+// for (let j = 1; j <= 10; j++) {
+
+//   finalOutput = star + "\n" + steric;
+//   console.log(finalOutput);
+// }
