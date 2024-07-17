722b07821e3eda1974e043bcafe47165468ce571
huzaifa ch <huzaifa.khuzaima47@gmail.com>
2024-07-17 11:36:59 +0500

    loopinloop

---
+++ assignment7.js
@@ -0,0 +1,21 @@
+let star = "*";
+let finalOutput = "";
+
+for (let j = 1; j <= 5; j++) {
+  {
+    for (let i = 1; i <= 5; i++) {
+     
+        finalOutput = finalOutput + star;
+      }
+      
+
+      }
+      
+
+      ]
+      finalOutput = finalOutput + star;
+    }
+  }
+  finalOutput = finalOutput + "\n";
+}
+console.log(finalOutput);
